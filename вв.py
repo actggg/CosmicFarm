8a96dd5953e75f48ed4245d9b2507a779bd3c1b0
actggg <120035486+actggg@users.noreply.github.com>
2023-03-19 13:21:59 +0300

    Add files via upload

---
+++ вв.py
@@ -0,0 +1,20 @@
+import requests
+import json
+
+
+def sorting(sub_li):
+    sub_li.sort(key=lambda x: x[1])
+    return sub_li
+mytoken = 'ntade86h'
+url = "https://dt.miet.ru/ppo_it_final"
+headers = {
+         "X-Auth-Token": mytoken
+    }
+response = requests.get(url, headers=headers)
+json_respons = response.json()
+r = []
+for i in json_respons['message']:
+    for j in i['points']:
+        r.append([j['SH'], j['distance']])
+sorting(r)
+print(r)
